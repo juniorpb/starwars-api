370f3a33716d8704752f9eed7b4eb394e1d9c555
juniorpb <juniorfoxld@hotmail.com>
2020-07-18 23:38:31 -0300

    create package utils

---
+++ src/main/java/com/b2w/starwars/services/utils/GenericResponse.java
@@ -0,0 +1,18 @@
+package com.b2w.starwars.services.utils;
+
+// generic return, example: success, error and similar messages
+public class GenericResponse {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
+
